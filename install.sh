247387682606f2fb57e659507030b6a6ee0fa3a2
Nguyen Thanh Duong (Harry) <nguyen.thanh.duong@azoom.jp>
2026-05-20 15:12:30 +0700

    :fix: Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ set -e
 
 INSTALL_DIR="/usr/local/bin"
 BINARY_NAME="copilothub"
-GITHUB_REPO="azoom-nguyen-thanh-duong/spec-designer"
+GITHUB_REPO="ddev94/copilothub"
 VERSION="${1:-latest}"
 
 echo "🚀 Spec Clarify Installer"
